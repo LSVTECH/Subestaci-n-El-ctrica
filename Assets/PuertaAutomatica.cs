6d9295c16acd1c89ac03a99264bab9cbd927fb5f
Eduardo De Jesús Mogollón Salcedo <70734710+Krost22@users.noreply.github.com>
2025-03-25 14:49:02 -0500

    Primeras pruebas occlusion culling

--- Assets/PuertaAutomatica.cs
+++ Assets/PuertaAutomatica.cs
@@ -3,44 +3,37 @@ using UnityEngine;
 public class PuertaAutomatica : MonoBehaviour
 {
     public GameObject PuertaIzq, PuertaDrc;
-    public Animator PuertaAnimIzq, PuertaAnimDrc;
+    private Animator PuertaAnimIzq, PuertaAnimDrc;
 
     private void Start()
     {
-        // Se obtiene el componente Animator de cada puerta
-        PuertaAnimIzq = PuertaIzq.GetComponent<Animator>();
-        PuertaAnimDrc = PuertaDrc.GetComponent<Animator>();
+        // Se obtiene y verifica el componente Animator de cada puerta
+        if (PuertaIzq != null) PuertaAnimIzq = PuertaIzq.GetComponent<Animator>();
+        if (PuertaDrc != null) PuertaAnimDrc = PuertaDrc.GetComponent<Animator>();
+
+        if (PuertaAnimIzq == null || PuertaAnimDrc == null)
+        {
+            Debug.LogError("Uno o más Animator no fueron encontrados en las puertas.");
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            PuertaAnimDrc.SetBool("Player", true);
-            PuertaAnimIzq.SetBool("Player", true);
-            Debug.Log("Abriendo puerta");
-        }
-        else
-        {
-            PuertaAnimDrc.SetBool("Player", false);
-            PuertaAnimIzq.SetBool("Player", false);
-            Debug.Log("Cerrando puerta");
+            PuertaAnimDrc?.SetBool("Player", true);
+            PuertaAnimIzq?.SetBool("Player", true);
+           // Debug.Log("Abriendo puerta");
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            PuertaAnimDrc.SetBool("Player", false);
-            PuertaAnimIzq.SetBool("Player", false);
-            Debug.Log("Abriendo puerta");
-        }
-        else
-        {
-            PuertaAnimDrc.SetBool("Player", true);
-            PuertaAnimIzq.SetBool("Player", true);
-            Debug.Log("Cerrando puerta");
+            PuertaAnimDrc?.SetBool("Player", false);
+            PuertaAnimIzq?.SetBool("Player", false);
+           // Debug.Log("Cerrando puerta");
         }
     }
 }
